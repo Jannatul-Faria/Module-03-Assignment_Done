8d6f902f6db6b9c61e5228f8948347f4fbb8fa9f
jannatul <kairasana8@gmail.com>
2023-09-26 21:12:32 +0600

    done

--- task_02.php
+++ task_02.php
@@ -5,7 +5,24 @@
 Create an array called $numbers containing the numbers 1 to 10. Write a PHP function which takes the "$numbers" array as an 
 argument to remove the even numbers from the array and print the resulting array.*/
 
-function removeEvenNumber($numbers){
+// function removeEvenNumber($numbers){
+   // $oddNumber=[];
+   //  foreach($numbers as $number){
+      //  if ($number%2!==0) {
+         // $oddNumber[]=$number;
+      //  }
+   //  }
+   //  return $oddNumber;
+   //  
+   //  
+// }
+// $numbers=range(1,10);
+// $resultingNumber=removeEvenNumber($numbers);
+// print_r($resultingNumber);
+// 
+
+
+function removeNumber($numbers){
    $oddNumber=[];
     foreach($numbers as $number){
        if ($number%2!==0) {
@@ -17,5 +34,7 @@ function removeEvenNumber($numbers){
     
 }
 $numbers=range(1,10);
-$resultingNumber=removeEvenNumber($numbers);
-print_r($resultingNumber);
+$result=removeNumber($numbers);
+print_r($result);
+
+
--- task_04.php
+++ task_04.php
@@ -10,11 +10,17 @@ three subjects: Math, English, and Science. Write a PHP function which takes "$s
  function averageGrade($studentGrades){
     foreach ($studentGrades as $index=>$grade) {
         $averageGrade=array_sum ($grade)/count($grade);
-        echo "The average grade for  student $index: $averageGrade \n"  ;
+        
+        echo "The average grade for $index: $averageGrade \n"  ;
     }
 }
 
  
 
-$studentGrades=[['math'=>78,'english'=>85,'scince'=>90],['math'=>85,'english'=>86,'scince'=>89],['math'=>75,'english'=>92,'scince'=>85]];
+$studentGrades=[
+    'student1'=>['math'=>78,'english'=>85,'scince'=>90],
+    'student2'=>['math'=>85,'english'=>86,'scince'=>89],
+    'student3'=>['math'=>75,'english'=>92,'scince'=>85]
+];
+
 averageGrade($studentGrades);
