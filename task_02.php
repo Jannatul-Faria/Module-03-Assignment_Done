1cdfc0bf498dfd2b2a9f463f974ab3f2380466c0
jannatul <kairasana8@gmail.com>
2023-09-27 20:30:52 +0600

    ok

--- task_02.php
+++ task_02.php
@@ -33,7 +33,7 @@ function removeNumber($numbers){
     
     
 }
-$numbers=range(1,10);
+$numbers=[1,2,3,4,5,6,7,8,9,10];
 $result=removeNumber($numbers);
 print_r($result);
 
--- task_04.php
+++ task_04.php
@@ -5,22 +5,41 @@
 Create a multidimensional array called $studentGrades to store the grades of three students. Each student has grades for 
 three subjects: Math, English, and Science. Write a PHP function which takes "$studentGrades" as an argument to calculate
  and print the average grade for each student.*/
-
- 
- function averageGrade($studentGrades){
-    foreach ($studentGrades as $index=>$grade) {
-        $averageGrade=array_sum ($grade)/count($grade);
-        
-        echo "The average grade for $index: $averageGrade \n"  ;
+function averageGrade($studentGrades){
+    $averageGrades=[];
+   $index=[];
+    
+    foreach ($index as $key => $value) {
+      $name=$key;
+      $index[]=$name;
     }
-}
+    foreach ($studentGrades as $index=>$grade) {
+      $averageGrade=array_sum ($grade)/count($grade);
+      $averageGrades[]=$averageGrade;
+    }
 
- 
-
+    foreach ($averageGrades as $index=>$averageGrade) {
+      if ($averageGrade >= 80) {
+       echo "The average grade for student $index: $averageGrade and  grade is A+. \n"  ;
+      } elseif ($averageGrade >= 70) {
+       echo "The average grade for student $index: $averageGrade and  grade is A.\n"  ;
+      } elseif ($averageGrade >= 60) {
+       echo "The average grade for student $index: $averageGrade and  grade is B. \n"  ;
+      } elseif ($averageGrade >= 50) {
+       echo "The average grade for student $index: $averageGrade and  grade is C. \n"  ;
+      } elseif ($averageGrade >= 40) {
+       echo "The average grade for student $index: $averageGrade and  grade is D. \n"  ;
+      } else {
+       echo "The average grade for student $index: $averageGrade and  grade is F. \n"  ;
+      }
+    }
+}  
 $studentGrades=[
-    'student1'=>['math'=>78,'english'=>85,'scince'=>90],
-    'student2'=>['math'=>85,'english'=>86,'scince'=>89],
-    'student3'=>['math'=>75,'english'=>92,'scince'=>85]
+           ['math'=>78,'english'=>85,'scince'=>90],
+           ['math'=>85,'english'=>57,'scince'=>89],
+           ['math'=>75,'english'=>72,'scince'=>45]
 ];
 
 averageGrade($studentGrades);
+
+
